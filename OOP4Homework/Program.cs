c40bdeb003dba44591deeef8d3cf842eb374c933
Natalya <soltk7@yandex.ru>
2021-10-24 17:12:50 +0300

    ДЗ к 4 уроку

--- OOP4Homework/Program.cs
+++ OOP4Homework/Program.cs
@@ -6,7 +6,12 @@ namespace OOP4Homework
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Build build1 = new Build(15, 5, 100, 5);
+            build1.GetBuild();
+            Build build2 = new Build(15, 5, 100, 5);
+            build2.GetBuild();
+            Console.WriteLine($"Высота этажа: {build2.HeightFloor()}  Количество квартир в подъезде: {build2.CountApartmentsInEntrance()} " +
+                $"Количество квартир на этаже: {build2.CountApartmentsOnFloor()}");
         }
     }
 }
