deb9d05d99a9ff55bc7321f946fd8f0292456ef7
Natalya <soltk7@yandex.ru>
2021-10-24 22:55:30 +0300

    2й пункт ДЗ4

---
+++ OOP4Homework/Creator.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP4Homework
+{
+      class Creator
+
+    {
+        // спрятала класс Здание внутрь класса фабрики -- не совсем уверена, что требовалось именно это, но условие поняла так, что все должно быть скрыто и недоступно извне
+        private class Build
+        {
+            private static int number = 1;
+
+            public int Number { get; set; }
+
+            public double Height { get; set; }
+
+            public int Floors { get; set; }
+
+            public int Apartments { get; set; }
+
+            public int Entrances { get; set; }
+
+            public int Code { get; set; }
+
+            // конструктор
+
+            public Build(double height, int floors, int apartments, int entrances)
+            {
+                this.Height = height;
+                this.Floors = floors;
+                this.Apartments = apartments;
+                this.Entrances = entrances;
+                this.Number = number;
+                this.Code = 0;
+                ++number;
+
+            }
+
+            public override int GetHashCode()
+            {
+                int a = base.GetHashCode();
+                this.Code = a;
+                return a;
+            }
+
+
+            // высота этажа
+            public double HeightFloor()
+            {
+                return this.Height / this.Floors;
+            }
+
+            // количество квартир в подъезде
+
+            public int CountApartmentsInEntrance()
+            {
+                return this.Apartments / this.Entrances;
+            }
+
+            // количество квартир на этаже
+            public int CountApartmentsOnFloor()
+            {
+                int c = this.CountApartmentsInEntrance();
+                return c / this.Floors;
+            }
+
+
+
+
+        }
+
+        // конструктор сделан приватным
+        private Creator() { }
+
+        // хэш-таблица для хранения объектов
+        private static HashSet<Build> hashSet = new HashSet<Build>();
+
+        // создание здания и добавление записи в хэш-таблицу
+        public static void CreateBuild(double height, int floors, int apartments, int entrances)
+        {
+           Build build = new Build(height, floors, apartments, entrances);
+            hashSet.Add(build);          
+            
+        }
+        // печать хэш-таблицы
+        public static void PrintBuilds()
+        {
+            foreach(Build b in hashSet)
+            {
+                Console.WriteLine($"Хэш код: {b.Code}  Номер здания: {b.Number}  Высота: {b.Height}  Количество этажей: {b.Floors} Количество квартир: {b.Apartments}  Количество подъездов: {b.Entrances} ");
+                Console.WriteLine();
+            }
+        }
+
+        // удаление по уникальному номеру
+        public static void DeleteBuild(int n)
+        {
+
+            foreach(Build b in hashSet)
+            {
+                if(b.Number==n)
+                {
+                    hashSet.Remove(b);
+                }
+            }
+
+        }
+         
+        // запрос параметров по номеру здания
+
+        public static void InfoBuild(int n)
+        {
+            foreach (Build b in hashSet)
+            {
+                if (b.Number == n)
+                {
+                    Console.WriteLine($"Высота этажа: {b.HeightFloor()}  Количество квартир в подъезде: {b.CountApartmentsInEntrance()} " +
+                $"Количество квартир на этаже: {b.CountApartmentsOnFloor()}");
+                }
+            }
+
+        }
+
+
+
+    }
+
+    
+}
--- OOP4Homework/Program.cs
+++ OOP4Homework/Program.cs
@@ -6,12 +6,23 @@ namespace OOP4Homework
     {
         static void Main(string[] args)
         {
-            Build build1 = new Build(15, 5, 100, 5);
-            build1.GetBuild();
-            Build build2 = new Build(15, 5, 100, 5);
-            build2.GetBuild();
-            Console.WriteLine($"Высота этажа: {build2.HeightFloor()}  Количество квартир в подъезде: {build2.CountApartmentsInEntrance()} " +
-                $"Количество квартир на этаже: {build2.CountApartmentsOnFloor()}");
+            // доавление зданий
+           Creator.CreateBuild(15, 5, 100, 5);
+           Creator.CreateBuild(15, 5, 100, 5);
+           Creator.CreateBuild(15, 5, 100, 5);
+           Creator.CreateBuild(15, 5, 100, 5);
+            // вывод хэш-таблицы
+            Creator.PrintBuilds();
+
+            Console.WriteLine("Удаление здания с номером 3");
+            Creator.DeleteBuild(3);
+            // вывод хэш-таблицы
+
+            Creator.PrintBuilds();
+
+            // печать информации о 2м здании
+            Creator.InfoBuild(2);
+
         }
     }
 }
